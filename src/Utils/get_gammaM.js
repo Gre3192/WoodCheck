600b6409184aeb0b3617eec3beafbb8d25860040
Gre3192 <gre.distefano@gmail.com>
2024-09-24 19:03:22 +0200

    Added new condition to return 1 for 'eccezzionali' wood type in get_gammaM function.

--- src/Utils/get_gammaM.js
+++ src/Utils/get_gammaM.js
@@ -10,6 +10,7 @@ export default function get_gammaM(woodType = 'lamellare', columnTableNTC = 'A')
         else if (woodType?.toLowerCase().includes('compensato')) return 1.40
         else if (woodType?.toLowerCase().includes('pannelli di scaglie')) return 1.40
         else if (woodType?.toLowerCase().includes('unioni')) return 1.50
+        else if (woodType?.toLowerCase().includes('eccezzionali')) return 1
     }
     else if (columnTableNTC?.toUpperCase() === 'B') {
         
@@ -20,6 +21,7 @@ export default function get_gammaM(woodType = 'lamellare', columnTableNTC = 'A')
         else if (woodType?.toLowerCase().includes('compensato')) return 1.30
         else if (woodType?.toLowerCase().includes('pannelli di scaglie')) return 1.30
         else if (woodType?.toLowerCase().includes('unioni')) return 1.40
+        else if (woodType?.toLowerCase().includes('eccezzionali')) return 1
     }
 
 }
