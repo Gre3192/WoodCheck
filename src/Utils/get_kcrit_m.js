34a9c35934f7ff8f8c4473a2dd1d9e3cd27ec195
Gre3192 <gre.distefano@gmail.com>
2024-09-25 02:44:23 +0200

    "Updated LaTeX syntax in get_kcrit_m.js by adding dollar signs around lambda_rel_m_y and lambda_rel_m_z in kcrit_m_y_description and kcrit_m_z_description."

--- src/Utils/get_kcrit_m.js
+++ src/Utils/get_kcrit_m.js
@@ -17,9 +17,9 @@ export default function get_kcrit_m(lambda_rel_m_y, lambda_rel_m_z) {
             : 0.75 < lambda_rel_m_y && lambda_rel_m_y <= 1.4 ? `1.56-0.75\\cdot${lambda_rel_m_y} = `
                 : `\\dfrac{1}{${lambda_rel_m_y}^2} = `,
 
-        kcrit_m_y_description: lambda_rel_m_y <= 0.75 ? `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a z per \\lambda_{rel,m,y}\\leq 0.75`
-            : 0.75 < lambda_rel_m_y && lambda_rel_m_y <= 1.4 ? `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a z per 0.75<\\lambda_{rel,m,y}\\leq 1.4`
-                : `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a z per $\\lambda_{rel,m,y}> 1.4$`,
+        kcrit_m_y_description: lambda_rel_m_y <= 0.75 ? `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a z per $\\lambda_{rel,m,y}\\leq 0.75$`
+            : 0.75 < lambda_rel_m_y && lambda_rel_m_y <= 1.4 ? `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a z per $0.75<\\lambda_{rel,m,y}\\leq 1.4$`
+                : `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a z per $\\lambda_{rel,m,y} > 1.4$`,
 
 
 
@@ -37,9 +37,9 @@ export default function get_kcrit_m(lambda_rel_m_y, lambda_rel_m_z) {
             : 0.75 < lambda_rel_m_z && lambda_rel_m_z <= 1.4 ? `1.56-0.75\\cdot${lambda_rel_m_z} = `
                 : `\\dfrac{1}{${lambda_rel_m_z}^2} = `,
 
-        kcrit_m_z_description: lambda_rel_m_z <= 0.75 ? `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a y per \\lambda_{rel,m,z}\\leq 0.75`
-            : 0.75 < lambda_rel_m_z && lambda_rel_m_z <= 1.4 ? `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a y per 0.75<\\lambda_{rel,m,z}\\leq 1.4`
-                : `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a y per $\\lambda_{rel,m,z}> 1.4$`,
+        kcrit_m_z_description: lambda_rel_m_z <= 0.75 ? `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a y per $\\lambda_{rel,m,z}\\leq 0.75$`
+            : 0.75 < lambda_rel_m_z && lambda_rel_m_z <= 1.4 ? `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a y per $0.75<\\lambda_{rel,m,z}\\leq 1.4$`
+                : `Coefficiente riduttivo di tensione critica per instabilità di trave attorno a y per $\\lambda_{rel,m,z} > 1.4$`,
 
     }
 }
