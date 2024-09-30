afdc4757303ece3c48298dc9c66b6aa4ab6e7479
Gre3192 <gre.distefano@gmail.com>
2024-10-01 01:54:14 +0200

    "Updated formulaVal for sigma_m_crit_y to include custom decimal values for Mcrit_y and Wel_y"

--- src/Utils/get_sig_m_crit.js
+++ src/Utils/get_sig_m_crit.js
@@ -8,7 +8,7 @@ export default function get_sig_m_crit(Mcrit_y, Mcrit_z, Wel_y, Wel_z) {
         sigma_m_crit_y: Mcrit_y / Wel_y,
         sigma_m_crit_y_title: `\\sigma_{m,y,crit} = `,
         sigma_m_crit_y_formula: `\\dfrac{M_{y,crit}}{W_{el,y}} = `,
-        sigma_m_crit_y_formulaVal: `\\dfrac{M_{y,crit}}{W_{el,y}} = `,
+        sigma_m_crit_y_formulaVal: `\\dfrac{${customDecimal(Mcrit_y)}}{${customDecimal(Wel_y)}} = `,
         sigma_m_crit_y_description: `Tensione critica per flessione attorno all'asse y`,
 
         sigma_m_crit_z: Mcrit_z / Wel_z,
